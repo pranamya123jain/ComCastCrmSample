6efa7c410af59e79a6c49cb3a0797ec96f8eb560
LENOVO <LENOVO@DESKTOP-BL08O2D>
2024-11-06 13:12:54 +0530

    new comment

--- ComCastCrmSample/src/test/java/practice/practiceClass.java
+++ ComCastCrmSample/src/test/java/practice/practiceClass.java
@@ -10,5 +10,6 @@ public class practiceClass {
 		System.out.println("Hello");
 		System.out.println("break");
 		System.out.println("new statement");
+		System.out.println("new more statement");
 	}
 }
